fffd71c590b250c9f9fa790ee8b3a2b0d08da103
Huvinesh Rajendran <huvineshrajendran@icloud.com>
2026-01-23 02:07:28 +0800

    use actual shasum

--- Formula/gitid.rb
+++ Formula/gitid.rb
@@ -2,7 +2,7 @@ class Gitid < Formula
   desc "CLI tool for seamlessly managing multiple Git identities across GitHub and GitLab"
   homepage "https://github.com/huvinesh/gitid"
   url "https://github.com/huvinesh/gitid/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "placeholder_sha256"
+  sha256 "bc3fac549eedd31b3a9b8750228fd94496e4c761eb6d83eb7dc87486afb0424b  gitid-0.1.0.tar.gz"
   license "MIT"
 
   depends_on "rust" => :build
